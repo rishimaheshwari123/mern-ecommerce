0556bb093f98a3a7c3e6c815b3aef3f1ac86abb7
Rishi <rishimaheshwari040@gmail.com>
2023-10-04 15:12:33 +0530

    first commit

---
+++ client/src/components/Prices.jsx
@@ -0,0 +1,42 @@
+export const Prices = [
+  {
+    _id: 0,
+    name: "₹0 to ₹99",
+    array: [0, 99],
+  },
+  {
+    _id: 1,
+    name: "₹99 to ₹199",
+    array: [99, 199],
+  },
+  {
+    _id: 2,
+    name: "₹199 to ₹299",
+    array: [199, 299],
+  },
+  {
+    _id: 3,
+    name: "₹299 to ₹399",
+    array: [299, 399],
+  },
+  {
+    _id: 4,
+    name: "₹399 to ₹499",
+    array: [399, 499],
+  },
+  {
+    _id: 4,
+    name: "₹499 to ₹599",
+    array: [499, 599],
+  },
+  {
+    _id: 5,
+    name: "₹599 to ₹799",
+    array: [799, 799],
+  },
+  {
+    _id: 6,
+    name: "₹799 or more",
+    array: [799, 9999],
+  },
+];
